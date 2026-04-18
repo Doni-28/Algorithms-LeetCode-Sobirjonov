b1db03200dbb7969135fe0a574a390b12d68b94e
Doni-28 <129668089+Doni-28@users.noreply.github.com>
2026-04-18 12:29:38 +0500

    Time: 907 ms (52.91%) | Memory: 13.1 MB (43.16%) - LeetSync

---
+++ 322-coin-change/coin-change.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def coinChange(self, coins, amount):
+        """
+        :type coins: List[int]
+        :type amount: int
+        :rtype: int
+        """
+        
+        # dp[i] — минимальное количество монет для суммы i
+        dp = [float('inf')] * (amount + 1)
+        dp[0] = 0  # базовый случай
+        
+        # перебираем все суммы
+        for i in range(1, amount + 1):
+            for coin in coins:
+                if i - coin >= 0:
+                    dp[i] = min(dp[i], dp[i - coin] + 1)
+        
+        # если не смогли собрать сумму
+        return dp[amount] if dp[amount] != float('inf') else -1
